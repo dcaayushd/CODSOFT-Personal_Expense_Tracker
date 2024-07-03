cd1284920670ef686482f03d0dfc5aa129184c41
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-03 19:41:35 +0545

    Category Badge Added

---
+++ lib/components/category_badge.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+import 'package:personal_expense_tracker/models/category.dart';
+
+class CategoryBadge extends StatelessWidget {
+  final Category? category;
+
+  const CategoryBadge({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(6, 2, 6, 2),
+      decoration: BoxDecoration(
+        color: category?.color.withAlpha(102) ?? CupertinoColors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        category?.name ?? "Unknown",
+        style: TextStyle(
+          color: category?.color ?? CupertinoColors.black,
+          fontSize: 13,
+        ),
+      ),
+    );
+  }
+}
