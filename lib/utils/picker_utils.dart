0aeb57653b6ba54d65ed77c962b4ecbbf2ca144e
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-03 16:25:55 +0545

    Color Pickewr Utils Modified

---
+++ lib/utils/picker_utils.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+void showPicker(BuildContext context, Widget child) {
+  showCupertinoModalPopup<void>(
+    context: context,
+    builder: (BuildContext context) => Container(
+      height: 216,
+      padding: const EdgeInsets.only(top: 6.0),
+      margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      color: CupertinoColors.systemBackground.resolveFrom(context),
+      child: SafeArea(
+        top: false,
+        child: child,
+      ),
+    ),
+  );
+}
