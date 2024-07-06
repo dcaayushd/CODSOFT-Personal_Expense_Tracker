01bde296084fde78d53be4d47cb6858a64781675
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-06 18:01:02 +0545

    Realm Modified to Set the Budget

--- lib/realm.dart
+++ lib/realm.dart
@@ -1,7 +1,8 @@
 import 'package:realm/realm.dart';
 
+import 'models/budget.dart';
 import 'models/expense.dart';
 import 'models/category.dart';
 
-var _config = Configuration.local([Expense.schema, Category.schema]);
+var _config = Configuration.local([Expense.schema, Category.schema, Budget.schema]);
 var realm = Realm(_config);
